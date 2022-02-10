f9e57739704cfbcfbe24bae8bf4d589587fb4f7d
Adam Davis <adavisdeveloper@gmail.com>
2022-02-09 21:43:38 -0500

    + Firestore 4 guestbook (works on local FS CLI)

---
+++ guestbook.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	addDoc,
+	getDoc,
+	updateDoc,
+	doc,
+	deleteDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
+
+const firebaseConfig = {
+	apiKey: apiKey(),
+
+	authDomain: "adamsland-guestbook.firebaseapp.com",
+
+	projectId: "adamsland-guestbook",
+
+	storageBucket: "adamsland-guestbook.appspot.com",
+
+	messagingSenderId: "994225521602",
+
+	appId: "1:994225521602:web:637ce4785ba4fa40b3cc5b",
+
+	measurementId: "G-YPXBDZMZS4",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+
+const apiKey = () => {
+	try {
+		return FIRESTORE_API_KEY;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+async function getEntries() {
+	const querySnapshot = await getDocs(collection(db, "guestbook-entries"));
+	querySnapshot.forEach((doc) => {
+		console.log(doc.id, "=>", doc.data());
+	});
+}
+
+if (FIRESTORE_API_KEY) getEntries();
+//getEntries();
