6572a012866ca189d6cc2b223f586575e1458d2d
Adam Davis <adavisdeveloper@gmail.com>
2022-02-12 19:08:11 -0500

    get firestore api working w/ netlify cloud funcs env vars

--- guestbook.js
+++ guestbook.js
@@ -1,63 +1,48 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
-// import {
-// 	getFirestore,
-// 	collection,
-// 	getDocs,
-// 	addDoc,
-// 	getDoc,
-// 	updateDoc,
-// 	doc,
-// 	deleteDoc,
-// } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	addDoc,
+	getDoc,
+	updateDoc,
+	doc,
+	deleteDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
 
-// const firebaseConfig = {
-// 	apiKey: FIRESTORE_API_KEY,
+const firebaseConfig = {
+	//ik this is bad
+	apiKey: apiKey().then((response) => response),
 
-// 	authDomain: "adamsland-guestbook.firebaseapp.com",
+	authDomain: "adamsland-guestbook.firebaseapp.com",
 
-// 	projectId: "adamsland-guestbook",
+	projectId: "adamsland-guestbook",
 
-// 	storageBucket: "adamsland-guestbook.appspot.com",
+	storageBucket: "adamsland-guestbook.appspot.com",
 
-// 	messagingSenderId: "994225521602",
+	messagingSenderId: "994225521602",
 
-// 	appId: "1:994225521602:web:637ce4785ba4fa40b3cc5b",
+	appId: "1:994225521602:web:637ce4785ba4fa40b3cc5b",
 
-// 	measurementId: "G-YPXBDZMZS4",
-// };
+	measurementId: "G-YPXBDZMZS4",
+};
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore();
-
-// const querySnapshot = await getDocs(collection(db, "guestbook-entries"));
-// await querySnapshot.forEach((doc) => {
-// 	console.log(doc.id, "=>", doc.data());
-// });
-
-// const apiKey = () => {
-// 	try {
-// 		return process.env.FIRESTORE_API_KEY;
-// 	} catch (error) {
-// 		console.error(error);
-// 	}
-// };
-
-// async function getFSEntries() {
-// 	const response = await fetch("/api/");
-// 	if (!response.ok) console.error("could not reach firestore api");
-// 	else return response.json();
-// }
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
 
 async function getEntries() {
+	const querySnapshot = await getDocs(collection(db, "guestbook-entries"));
+	await querySnapshot.forEach((doc) => {
+		console.log(doc.id, "=>", doc.data());
+	});
+}
+
+async function apiKey() {
 	const res = await fetch("/.netlify/functions/firestore-api");
 	return res.json();
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
-	console.log("Content loaded");
-	getEntries()
-		.then((response) => response)
-		.then((data) => console.log(data))
-		.catch((err) => console.log(err));
+	getEntries();
 });
