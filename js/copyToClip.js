f700510564ae750374d70e5ebd26253e513ea7b9
Adam Davis <adavisdeveloper@gmail.com>
2024-04-10 23:53:39 -0400

    copy to clipboard for adamsland button - working

--- js/copyToClip.js
+++ js/copyToClip.js
@@ -1,13 +1,17 @@
 function copyTextToClipboard(text) {
     navigator.clipboard.writeText(text).then(() => {
       console.log('Text copied to clipboard');
+      alert("You've copied the code for this button to your clipboard! Paste it in your website's HTML to link to Adams Land! :-)")
     }).catch(err => {
       console.error('Error in copying text: ', err);
     });
   }
   
-  document.getElementById('copyImage').addEventListener('click', function() {
-    const textToCopy = "This is the text to be copied";
+  document.getElementById('AdamsLandButton').addEventListener('click', function() {
+    const textToCopy = `
+    <a href="https://adams.land">
+      <img src="https://adams.land/img/link-buttons/adamsland.png" alt="adams land 81x33 website share/link button">
+    </a>`;
     copyTextToClipboard(textToCopy);
   });
   
